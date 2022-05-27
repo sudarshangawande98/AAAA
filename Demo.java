bbc0fa6bf0ea588f50f67023bb54356073210649
Sudarshan Gawande <sudarshangawande98@gmail.com>
2022-05-27 17:11:28 +0530

    Demo.java created

---
+++ Demo.java
@@ -0,0 +1,23 @@
+ass Date{
+	    private int day;
+	        private int month;
+		    private int year;
+
+		        public Date(int day, int month, int year) {
+				        this.day = day;
+					        this.month = month;
+						        this.year = year;
+							    }
+
+			    public String toString) {
+				            return (day +" "+month+" "+year);
+					        }
+}
+p(ublic class Program {
+	    public static void main(String[] args) {
+		            Date date = null; //new Date(22,03,1996)
+        System.out.println(date.toString());
+	    }
+}
+
+
